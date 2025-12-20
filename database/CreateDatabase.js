b99683c13f2b02b926bcc23683737fefbe6f5bfb
Renan-Silva235 <renan.rubio94@email.com>
2025-12-20 16:37:15 -0300

    create database and tables

--- database/CreateDatabase.js
+++ database/CreateDatabase.js
@@ -1,3 +1,66 @@
-const Database = require("better-sqlite3");
+import Database from "better-sqlite3";
+import path from "path";
+import { fileURLToPath } from "url";
 
-const db = new Database("./database.db");
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = path.dirname(__filename);
+const dbPath = path.resolve(__dirname, process.env.DATABASE_TEST)
+
+export default class CreateDatabase{
+    constructor(){
+        this.db = new Database(dbPath);
+        this.#createTableEmployees();
+        this.#createTableCustomer();
+        this.#createTableDonor();
+    }
+
+    #createTableEmployees(){
+        const commandSqlToCreateTable = `CREATE TABLE IF NOT EXISTS Employees(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            Nome TEXT NOT NULL,
+            Idade INTEGER NOT NULL,
+            DataDeNascimento TEXT NOT NULL,
+            Cpf TEXT UNIQUE NOT NULL,
+            Email TEXT UNIQUE NOT NULL,
+            Senha TEXT NOT NULL,
+            Cargo TEXT NOT NULL
+        );`;
+        this.db.exec(commandSqlToCreateTable);
+    }
+
+    #createTableCustomer(){
+        const commandSqlToCreateTable = `CREATE TABLE IF NOT EXISTS Customers(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_responsavel INTEGER NOT NULL,
+            Nome TEXT NOT NULL,
+            Idade INTEGER NOT NULL,
+            DataDeNascimento TEXT NOT NULL,
+            Cpf TEXT UNIQUE NOT NULL,
+            Email TEXT UNIQUE NOT NULL,
+            Senha TEXT NOT NULL,
+            Endereco TEXT NOT NULL,
+            Cidade TEXT NOT NULL,
+            Estado TEXT NOT NULL,
+
+            FOREIGN KEY (id_responsavel) REFERENCES Employees(id)
+        );`;
+        this.db.exec(commandSqlToCreateTable);
+    }
+
+    #createTableDonor(){
+        const commandSqlToCreateTable = `CREATE TABLE IF NOT EXISTS Donors(
+            id INTEGER PRIMARY KEY AUTOINCREMENT,
+            id_responsavel INTEGER NOT NULL,
+            Nome TEXT NOT NULL,
+            Idade INTEGER NOT NULL,
+            DataDeNascimento TEXT NOT NULL,
+            Cpf TEXT UNIQUE NOT NULL,
+            Email TEXT UNIQUE NOT NULL,
+
+            FOREIGN KEY (id_responsavel) REFERENCES Employees(id)
+        );`
+
+        this.db.exec(commandSqlToCreateTable);
+    }
+
+}
